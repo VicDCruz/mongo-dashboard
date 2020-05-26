7c20e34df39cb03e704b357a12301de3277ba4da
Victor Cruz <vicdan215@gmail.com>
2020-05-26 13:54:07 -0500

    No Need

--- real-time-dashboard/index.js
+++ real-time-dashboard/index.js
@@ -15,32 +15,3 @@ server.listen().then(({ version, port }) => {
   console.error('Fatal error during server start: ');
   console.error(e.stack || e);
 });
-
-// require('dotenv').config();
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const CubejsServerCore = require('@cubejs-backend/server-core');
-
-// const app = express();
-// app.use(require('cors')());
-// app.use(bodyParser.json({ limit: '50mb' }));
-
-// // const serverCore = CubejsServerCore.create();
-// const serverCore = new CubejsServerCore({
-//     processSubscriptionsInterval: 1,
-//     orchestratorOptions: {
-//       queryCacheOptions: {
-//         refreshKeyRenewalThreshold: 1,
-//       }
-//     }
-//   });
-// serverCore.initApp(app);
-
-// const port = process.env.PORT || 4000;
-// app.listen(port, (err) => {
-//   if (err) {
-//     console.error('Fatal error during server start: ');
-//     console.error(e.stack || e);
-//   }
-//   console.log(`🚀 Cube.js server (${CubejsServerCore.version()}) is listening on ${port}`);
-// });
