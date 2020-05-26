35ff60bdf05cb64120ee045b59d2dd9e121cbf51
Victor Cruz <vicdan215@gmail.com>
2020-05-26 12:38:25 -0500

    Change logic

--- dashboard-app/app.js
+++ dashboard-app/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const port = process.env.NODE_ENV === 'production' ? 5000 : process.env.PORT ? process.env.PORT : 3000
+if (process.env.NODE_ENV === 'production') const port = 5000;
+else const port = process.env.PORT ? process.env.PORT : 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
