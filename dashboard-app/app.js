7f7a0f10926ee0cba338a883f77d865c9932a4a1
Victor Cruz <vicdan215@gmail.com>
2020-05-26 12:39:28 -0500

    Fix const bug

--- dashboard-app/app.js
+++ dashboard-app/app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-if (process.env.NODE_ENV === 'production') const port = 5000;
-else const port = process.env.PORT ? process.env.PORT : 3000;
+let port;
+if (process.env.NODE_ENV === 'production') port = 5000;
+else port = process.env.PORT ? process.env.PORT : 3000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
