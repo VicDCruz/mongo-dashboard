bfb341fd9a1d7c3b95f6c21580239091c29b4629
Victor Cruz <vicdan215@gmail.com>
2020-05-26 13:47:02 -0500

    Old version

--- real-time-dashboard/index.js
+++ real-time-dashboard/index.js
@@ -1,46 +1,46 @@
-// const CubejsServer = require('@cubejs-backend/server');
+const CubejsServer = require('@cubejs-backend/server');
 
-// const server = new CubejsServer({
-//   processSubscriptionsInterval: 1,
-//   orchestratorOptions: {
-//     queryCacheOptions: {
-//       refreshKeyRenewalThreshold: 1,
-//     }
-//   }
-// });
-
-// server.listen().then(({ version, port }) => {
-//   console.log(`🚀 Cube.js server (${version}) is listening on ${port}`);
-// }).catch(e => {
-//   console.error('Fatal error during server start: ');
-//   console.error(e.stack || e);
-// });
-
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const CubejsServerCore = require('@cubejs-backend/server-core');
-
-const app = express();
-app.use(require('cors')());
-app.use(bodyParser.json({ limit: '50mb' }));
-
-// const serverCore = CubejsServerCore.create();
-const serverCore = new CubejsServerCore({
-    processSubscriptionsInterval: 1,
-    orchestratorOptions: {
-      queryCacheOptions: {
-        refreshKeyRenewalThreshold: 1,
-      }
+const server = new CubejsServer({
+  processSubscriptionsInterval: 1,
+  orchestratorOptions: {
+    queryCacheOptions: {
+      refreshKeyRenewalThreshold: 1,
     }
-  });
-serverCore.initApp(app);
-
-const port = process.env.PORT || 4000;
-app.listen(port, (err) => {
-  if (err) {
-    console.error('Fatal error during server start: ');
-    console.error(e.stack || e);
   }
-  console.log(`🚀 Cube.js server (${CubejsServerCore.version()}) is listening on ${port}`);
 });
+
+server.listen().then(({ version, port }) => {
+  console.log(`🚀 Cube.js server (${version}) is listening on ${port}`);
+}).catch(e => {
+  console.error('Fatal error during server start: ');
+  console.error(e.stack || e);
+});
+
+// require('dotenv').config();
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const CubejsServerCore = require('@cubejs-backend/server-core');
+
+// const app = express();
+// app.use(require('cors')());
+// app.use(bodyParser.json({ limit: '50mb' }));
+
+// // const serverCore = CubejsServerCore.create();
+// const serverCore = new CubejsServerCore({
+//     processSubscriptionsInterval: 1,
+//     orchestratorOptions: {
+//       queryCacheOptions: {
+//         refreshKeyRenewalThreshold: 1,
+//       }
+//     }
+//   });
+// serverCore.initApp(app);
+
+// const port = process.env.PORT || 4000;
+// app.listen(port, (err) => {
+//   if (err) {
+//     console.error('Fatal error during server start: ');
+//     console.error(e.stack || e);
+//   }
+//   console.log(`🚀 Cube.js server (${CubejsServerCore.version()}) is listening on ${port}`);
+// });
